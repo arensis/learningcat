4476dd007c4d5ebad69b427929bfd0077b743995
Arensis <alfonso.madrid.dev@gmail.com>
2019-03-19 23:45:51 +0100

    Fixed deploy.sh file

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-DIR=$(dirname "$0")
-
-cd $DIR/..
-
 if [[ $(git status -s) ]]
 then
     echo "The working directory is dirty. Please commit any pending changes."
@@ -17,7 +13,7 @@ git worktree prune
 rm -rf .git/worktrees/public/
 
 echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public upstream/gh-pages
+git worktree add -B gh-pages public origin/gh-pages
 
 echo "Removing existing files"
 rm -rf public/*
@@ -34,7 +30,7 @@ fi
 git commit -m "$msg"
 
 echo "Commiting changes in gh-pages branch"
-git push origin master
+git push origin gh-pages
 
 echo "Back to the project folder"
 cd ..
