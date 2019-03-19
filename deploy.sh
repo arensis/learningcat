0109465233ea804cdbe751180d8b3b82998d89fa
Arensis <alfonso.madrid.dev@gmail.com>
2019-03-19 22:17:23 +0100

    Added deploy.sh file

---
+++ deploy.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+DIR=$(dirname "$0")
+
+cd $DIR/..
+
+if [[ $(git status -s) ]]
+then
+    echo "The working directory is dirty. Please commit any pending changes."
+    exit 1;
+fi
+
+echo "Deleting old publication"
+rm -rf public
+mkdir public
+git worktree prune
+rm -rf .git/worktrees/public/
+
+echo "Checking out gh-pages branch into public"
+git worktree add -B gh-pages public upstream/gh-pages
+
+echo "Removing existing files"
+rm -rf public/*
+
+echo "Generating site"
+hugo
+
+echo "Commiting changes in gh-pages branch"
+cd public && git add --all
+msg="Publishing to gh-pages `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+echo "Commiting changes in gh-pages branch"
+git push origin master
+
+echo "Back to the project folder"
+cd ..
